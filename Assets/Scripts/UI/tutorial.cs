d294cef4b49eb035662ac12c00e794a6087efdd6
Carlos <carlrd99@gmail.com>
2021-06-23 13:02:31 +0200

    tutorial cambio + postpo

--- Assets/Scripts/UI/tutorial.cs
+++ Assets/Scripts/UI/tutorial.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class tutorial : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class tutorial : MonoBehaviour
     public bool isShowingUp;
     public int cont;
     private float time = 1F;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -29,48 +31,59 @@ public class tutorial : MonoBehaviour
         if (isShowingUp)
         {
               //si apreta w aparece 1 texto y asi con cada texto y el contador se le suma 1
-            if (Input.GetKeyDown(KeyCode.W) && cont == 0)
-            {
-
+            if (Input.GetKeyDown(KeyCode.W) && cont == 0 || Input.GetKeyDown(KeyCode.A) && cont == 0 || Input.GetKeyDown(KeyCode.D) && cont == 0 || Input.GetKeyDown(KeyCode.S) && cont == 0
+                || Input.GetKeyDown(KeyCode.UpArrow) && cont == 0 || Input.GetKeyDown(KeyCode.DownArrow) && cont == 0 || Input.GetKeyDown(KeyCode.LeftArrow) && cont == 0 || Input.GetKeyDown(KeyCode.RightArrow) && cont == 0)
+            {  
                 StartCoroutine(ShowingText());
             }
-            if (Input.GetKeyDown(KeyCode.S) && cont == 1)
-            {
-
-                StartCoroutine(ShowingText());
-            }
-            if (Input.GetKeyDown(KeyCode.D) && cont == 2)
-            {
-
-                StartCoroutine(ShowingText());
-            }
-            if (Input.GetKeyDown(KeyCode.A) && cont == 3)
+            if (Input.GetKey(GameConstants.key_run) && cont == 1)
             {
                 StartCoroutine(ShowingText());
             }
-            if (Input.GetKeyDown(KeyCode.Mouse0) && cont == 4)
+            if (Input.GetKey(GameConstants.key_dash) && cont == 2)
             {
                 StartCoroutine(ShowingText());
             }
-            if (Input.GetKeyDown(KeyCode.Mouse1) && cont == 5)
+            if (Input.GetKey(GameConstants.key_attack) && cont == 3)
             {
                 StartCoroutine(ShowingText());
             }
-            if (Input.GetKeyDown(KeyCode.Mouse1) && cont == 6)
+            if (Input.GetKeyDown(KeyCode.Return) && cont == 4)
             {
                 StartCoroutine(ShowingText());
             }
-            if (Input.GetKeyDown(KeyCode.E) && cont == 7)
+            if (Input.GetKey(GameConstants.key_magic) && cont == 5)
             {
                 StartCoroutine(ShowingText());
             }
-            if (Input.GetKeyDown(KeyCode.Alpha1) && cont == 8)
+            if (Input.GetKey(GameConstants.key_cameraZoom) && cont == 6)
+            {
+                StartCoroutine(ShowingText());
+            }
+            if (Input.GetKey(GameConstants.key_magic) && cont == 7)
+            {
+                StartCoroutine(ShowingText());
+            }
+            if (Input.GetKey(GameConstants.key_barrier) && cont == 8)
+            {
+                StartCoroutine(ShowingText());
+            }
+            if (Input.GetKey(GameConstants.key_interact) && cont == 9)
+            {
+                StartCoroutine(ShowingText());
+            }
+            if (Input.GetKey(GameConstants.key_inv1) && cont == 10)
             {
                 StartCoroutine(ShowingText());
             }
 
         }
         
+    }
+
+    public void cambiarEscena()
+    {
+        SceneManager.LoadSceneAsync("Madriguera");
     }
 
     //showing text sirve para que enseñe cada texto que se ponga
